2ad7bc3ed2ba1670e328acf3c4449a49a8474900
anonymous675 <belogolovnikas@gmail.com>
2023-05-21 15:12:15 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@
 // Use this file to add JavaScript to your project
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer( { alpha: true } );
+renderer.setClearColor( 0x000000, 0 );
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("canvas").appendChild(renderer.domElement);
 
